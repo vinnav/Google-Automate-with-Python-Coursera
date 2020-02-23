8803d71d3cf9427fd5cd680a8985730bd61fab9b
vinnav <vincenzo.navarra@gmail.com>
2020-02-23 14:19:57 +0000

    Course Completed

---
+++ 1-PythonCrashCourse/Week5/1-OOP/ex1.py
@@ -0,0 +1,14 @@
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = "red"
+
+violet = Flower()
+violet.color = "blue"
+
+this_pun_is_for_you = "I'm programming and so can you too"
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you) 
